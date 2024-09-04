f7ea27f521d1da633263d70d22473c635ee881b4
keshab Aryal <keshabaryal152@gmail.com>
2024-09-04 20:36:20 +0545

    Done with Day38

--- Day38/src/com/DSA/Main.java
+++ Day38/src/com/DSA/Main.java
@@ -1,7 +1,10 @@
 package com.DSA;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int[] Rool = new int[]{1,2,3,4,5};
+        System.out.println(Arrays.toString(Rool));
     }
 }
