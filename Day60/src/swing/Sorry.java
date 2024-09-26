758c88807f044709e06f13f73c1d09f46619b935
keshab Aryal <keshabaryal152@gmail.com>
2024-09-26 19:30:42 +0545

    Done with Day60

---
+++ Day60/src/swing/Sorry.java
@@ -0,0 +1,7 @@
+package swing;
+
+public class Sorry {
+    public static void main(String[] args) {
+        System.out.println("Just for the shake of streak I am writting this code. Sorry");
+    }
+}
