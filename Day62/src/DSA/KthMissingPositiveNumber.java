e4d6529e816ea512577616dcd2085bb869a6bb6f
keshab Aryal <keshabaryal152@gmail.com>
2024-09-29 07:20:39 +0545

    Done with Day62

--- Day62/src/DSA/KthMissingPositiveNumber.java
+++ Day62/src/DSA/KthMissingPositiveNumber.java
@@ -2,6 +2,34 @@ package DSA;
 
 public class KthMissingPositiveNumber {
     public static void main(String[] args) {
-        
+        int[] arr = new int[]{1,2,3,4};
+        int k = 2;
+        System.out.println(findMissing(arr,k));
+    }
+    public static int findMissing(int[]arr,int k)
+    {
+        int low, high,mid,ans;
+        low = 0;
+        high = arr.length-1;
+        while(low<=high)
+        {
+            mid = low +(high-low)/2;
+            ans = checkIfFits(arr,k,mid);
+            if(ans==0)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return high+k+1;
+    }
+    public static int checkIfFits(int[]arr,int k, int mid)
+    {
+        int missing;
+        missing = arr[mid]-(mid+1);
+        if(missing<=k)
+            return 0;
+        else
+            return 1;
+
     }
 }
