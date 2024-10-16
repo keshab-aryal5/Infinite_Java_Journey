efb1a243bdd2d44217fc39b3e090c5ae412b515b
keshab Aryal <keshabaryal152@gmail.com>
2024-10-16 22:33:40 +0545

    Done with Day80

---
+++ Day80/src/DSA/MainClass.java
@@ -0,0 +1,7 @@
+package DSA;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Writing this line of code for the shake of consistency only. sorry");
+    }
+}
