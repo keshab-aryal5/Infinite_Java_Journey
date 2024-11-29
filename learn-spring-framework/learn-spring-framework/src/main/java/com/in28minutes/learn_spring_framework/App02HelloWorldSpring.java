e26773cf91bd92a913af21a76eb814c4713bb28b
keshab Aryal <keshabaryal152@gmail.com>
2024-11-29 21:56:56 +0545

    Done with Day109

--- learn-spring-framework/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ learn-spring-framework/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -10,8 +10,12 @@ public class App02HelloWorldSpring {
 		//		var context = new AnnotationConfigApplicationContenxt(HelloWorldConfiguration.class)
 		//2: Configure the things that we want Spring to manage - @Configuration
 		// HelloWorld
-		context.getBean("name");
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("person2MethodCall"));
+		System.out.println(context.getBean("person3Parameters"));
+		System.out.println(context.getBean("address2"));
+		System.out.println(context.getBean(Address.class));
 	}
-
 }
--- learn-spring-framework/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ learn-spring-framework/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,11 +3,46 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age, Address address) {
+};
+
+record Address(String firstLine, String city) {
+};
 @Configuration
 public class HelloWorldConfiguration {
 
 	@Bean
 	public String name() {
 		return "Ranga";
+	}
+
+	@Bean
+	public int age() {
+		return 15;
+	}
+
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
+	}
+
+	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address());
+	}
+
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) {
+		return new Person(name, age, address3);
+	}
+
+	@Bean(name = "address2")
+	public Address address() {
+		return new Address("New Baneshwor", "Kathmandu");
+	}
+
+	@Bean(name = "address3")
+	public Address address() {
+		return new Address("Malarani", "Arghakhanchi");
 	}
 }
